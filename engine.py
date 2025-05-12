3bda8b6d2f658472cc4af53308d0c96039f0c63b
Aasees <aaseesbadesha13@gmail.com>
2025-05-12 15:14:24 -0400

    added more details to results page

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ import discogs_client
 from releases import Song
 import os
 from dotenv import find_dotenv, load_dotenv
+from discogs_client.models import Release
 
 # initialize apis
 discogs_token = os.getenv("DISCOGS_KEY")
@@ -11,7 +12,7 @@ class Engine():
     # searching for a song through discogs api using title and artist
     def search(self, title, artist):
         try:
-            search_results = d.search(title = title, artist = artist, type = 'single')
+            search_results = d.search(release_title = title, artist = artist, type = 'single')
         # resolve error where too many requests occur - specific to discogs searches
         except discogs_client.exceptions.HTTPError:
             print('Too many requests. Please wait a minute to search again.')
@@ -19,7 +20,10 @@ class Engine():
         
         # find song item to return
         for item in search_results.page(1):
-            try: 
-                return Song(item.title, item.artists[0].name, item.images[0]['uri'])
-            except (KeyError, AttributeError):
+            if isinstance(item, Release):
+                try: 
+                    return Song(item.tracklist[0].title, item.artists[0].name, item.images[0]['uri'], item.genres, item.year, item.country, item.styles)
+                except (KeyError, AttributeError) as e:
+                    continue
+            else:
                 continue
--- releases.py
+++ releases.py
@@ -1,14 +1,17 @@
 class Song():
-    def __init__(self, title, artist, image):
+    def __init__(self, title, artist, image, genres, year, country, subgenres):
         self.type = 'Song'
         self.title = title
         self.artist = artist
         self.image = image
+        self.genres = ", ".join(genres)
+        self.year = year
+        self.country = country
+        self.subgenres = ", ".join(subgenres)
 
     def play(self):
         search_result = self.search()
-
-        
+    
 
 # # parent for both song and album to inherit from
 # class Release():
